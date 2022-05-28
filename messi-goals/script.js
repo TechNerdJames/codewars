21ad1f71cad69b82822bde5d99ffb506d87e6f6f
James <jamesdryden7@gmail.com>
2022-05-28 15:40:08 -0400

    added codewars, messi-goals

---
+++ messi-goals/script.js
@@ -0,0 +1,16 @@
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+function goals(laLigaGoals, copaDelGoals, championsLeagueGoals) {
+	let sum = laLigaGoals + copaDelGoals + championsLeagueGoals;
+
+	console.log(sum);
+}
+
+goals(43, 10, 5);
