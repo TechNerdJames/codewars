099e66a2adbf510ed322e27efc43c95024f9ca84
James <jamesdryden7@gmail.com>
2022-06-19 17:37:23 -0400

    added codewars, build-tower

---
+++ build-tower/script.js
@@ -0,0 +1,28 @@
+// Build a pyramid-shaped tower given a positive integer number of floors. A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ",
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ",
+//   "    ***    ",
+//   "   *****   ",
+//   "  *******  ",
+//   " ********* ",
+//   "***********"
+// ]
+
+function towerBuilder(nFloors) {
+	const tower = [];
+	for (let i = 1; i <= nFloors; i++) {
+		tower.push('*'.repeat(i * 2 - 1));
+	}
+	console.log(tower);
+}
+towerBuilder(4);
